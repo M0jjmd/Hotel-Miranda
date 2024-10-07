62a8cd6938597795abbf49cc517d17eeb4a15315
M0jjmd <moamine517@gmail.com>
2024-10-07 13:16:13 +0200

    Finished header desktop version

--- scripts/openMenu.js
+++ scripts/openMenu.js
@@ -1,3 +1,5 @@
+let lastScrollTop = 0
+const header = document.getElementById('header')
 const menuButton = document.getElementById('header__menu')
 const linksList = document.getElementById('header__links')
 
@@ -6,5 +8,27 @@ menuButton.addEventListener('click', function () {
         linksList.style.display = 'block'
     } else {
         linksList.style.display = 'none'
+    }
+})
+
+window.addEventListener('scroll', () => {
+    const scrollTop = window.scrollY
+
+    if (scrollTop > lastScrollTop) {
+        header.classList.add('hidden')
+    }
+
+    lastScrollTop = scrollTop
+})
+
+document.addEventListener('mousemove', (e) => {
+    if (e.clientY <= 0) {
+        header.classList.remove('hidden')
+    }
+})
+
+window.addEventListener('scroll', () => {
+    if (lastScrollTop === 0) {
+        header.classList.remove('hidden')
     }
 })
